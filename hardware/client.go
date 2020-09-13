a1126d116d60ef156a8e782f03f3fea9a93b1b5e
Kelly Deng <kelly@packet.com>
2020-09-13 01:05:27 -0400

    add comments to hardware package
    Signed-off-by: Kelly Deng <kelly@packet.com>

--- hardware/client.go
+++ hardware/client.go
@@ -20,15 +20,19 @@ var (
 	facility         = flag.String("facility", env.Get("HEGEL_FACILITY", "onprem"), "The facility we are running in (mostly to connect to cacher)")
 )
 
+// Client acts as the messenger between Hegel and Cacher/Tink
 type Client interface {
 	ByIP(ctx context.Context, id string, opts ...grpc.CallOption) (Hardware, error)
 	Watch(ctx context.Context, id string, opts ...grpc.CallOption) (Watcher, error)
 }
 
+// Hardware is the interface for Cacher/Tink hardware types
 type Hardware interface {
 	Export() ([]byte, error)
 	ID() (string, error)
 }
+
+// Watcher is the interface for Cacher/Tink watch client types
 type Watcher interface {
 	Recv() (Hardware, error)
 }
@@ -57,6 +61,7 @@ type watcherTinkerbell struct {
 	client tink.HardwareService_WatchClient
 }
 
+// New returns a new hardware Client, configured appropriately according to the mode (Cacher or Tink) Hegel is running in
 func New() (Client, error) {
 	var hg Client
 
@@ -78,6 +83,7 @@ func New() (Client, error) {
 	return hg, nil
 }
 
+// ByIP retrieves from Cacher the piece of hardware with the specified IP
 func (hg clientCacher) ByIP(ctx context.Context, ip string, opts ...grpc.CallOption) (Hardware, error) {
 	in := &cacher.GetRequest{
 		IP: ip,
@@ -89,6 +95,7 @@ func (hg clientCacher) ByIP(ctx context.Context, ip string, opts ...grpc.CallOpt
 	return &hardwareCacher{hw}, nil
 }
 
+// Watch returns a Cacher watch client on the hardware with the specified ID
 func (hg clientCacher) Watch(ctx context.Context, id string, opts ...grpc.CallOption) (Watcher, error) {
 	in := &cacher.GetRequest{
 		ID: id,
@@ -100,6 +107,7 @@ func (hg clientCacher) Watch(ctx context.Context, id string, opts ...grpc.CallOp
 	return &watcherCacher{w}, nil
 }
 
+// ByIP retrieves from Tink the piece of hardware with the specified IP
 func (hg clientTinkerbell) ByIP(ctx context.Context, ip string, opts ...grpc.CallOption) (Hardware, error) {
 	in := &tink.GetRequest{
 		Ip: ip,
@@ -111,6 +119,7 @@ func (hg clientTinkerbell) ByIP(ctx context.Context, ip string, opts ...grpc.Cal
 	return &hardwareTinkerbell{hw}, nil
 }
 
+// Watch returns a Tink watch client on the hardware with the specified ID
 func (hg clientTinkerbell) Watch(ctx context.Context, id string, opts ...grpc.CallOption) (Watcher, error) {
 	in := &tink.GetRequest{
 		Id: id,
@@ -122,6 +131,7 @@ func (hg clientTinkerbell) Watch(ctx context.Context, id string, opts ...grpc.Ca
 	return &watcherTinkerbell{w}, nil
 }
 
+// Export formats the piece of hardware to be returned in responses to clients
 func (hw *hardwareCacher) Export() ([]byte, error) {
 	exported := &exportedHardwareCacher{}
 
@@ -132,6 +142,7 @@ func (hw *hardwareCacher) Export() ([]byte, error) {
 	return json.Marshal(exported)
 }
 
+// ID returns the hardware ID
 func (hw *hardwareCacher) ID() (string, error) {
 	hwJSON := make(map[string]interface{})
 	err := json.Unmarshal([]byte(hw.hardware.JSON), &hwJSON)
@@ -145,14 +156,17 @@ func (hw *hardwareCacher) ID() (string, error) {
 	return id, err
 }
 
+// Export formats the piece of hardware to be returned in responses to clients
 func (hw *hardwareTinkerbell) Export() ([]byte, error) {
 	return json.Marshal(util.HardwareWrapper{Hardware: hw.hardware})
 }
 
+// ID returns the hardware ID
 func (hw *hardwareTinkerbell) ID() (string, error) {
 	return hw.hardware.Id, nil
 }
 
+// Recv receives a piece of hardware from the Cacher watch client
 func (w *watcherCacher) Recv() (Hardware, error) {
 	hw, err := w.client.Recv()
 	if err != nil {
@@ -161,6 +175,7 @@ func (w *watcherCacher) Recv() (Hardware, error) {
 	return &hardwareCacher{hw}, nil
 }
 
+// Recv receives a piece of hardware from the Tink watch client
 func (w *watcherTinkerbell) Recv() (Hardware, error) {
 	hw, err := w.client.Recv()
 	if err != nil {
