0ba601be0f5c46a1f5e18897b3dcc09a0ccec4d1
Chris Doherty <chris.doherty4@gmail.com>
2022-04-19 21:55:55 -0400

    Prepare CLI logic for centralized configuration.
    At present flag and environment configuration is sprawled across the
    Hegel codebase. This commit provides a cobra Command and corresponding
    Viper construct to centralize configuration.
    
    A subsequent change will refactor the codebase to leverage the inputs
    gathered from this command.
    
    Signed-off-by: Chris Doherty <chris.doherty4@gmail.com>

---
+++ datamodel/datamodel.go
@@ -0,0 +1,9 @@
+/*
+Package datamodel defines constants for the various back-end storage mechanisms supported by hegel.
+*/
+package datamodel
+
+const (
+	TinkServer = "1"
+	Kubernetes = "kubernetes"
+)
